db4e171d716478478fceadfca47ed0010e83eadc
Doug Graiser <dmgraiser@gmail.com>
2017-09-11 22:51:29 -0500

    [#7] Cancan mostly working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170911163750) do
+ActiveRecord::Schema.define(version: 20170912033626) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20170911163750) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
